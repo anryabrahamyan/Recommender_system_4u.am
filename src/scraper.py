77c068a12e76eaff1dbd1b9cba4ce4c92681afef
anryabrahamyan <42089126+anryabrahamyan@users.noreply.github.com>
2022-11-01 09:53:10 +0400

    add sample output

--- src/scraper.py
+++ src/scraper.py
@@ -72,3 +72,37 @@ if __name__=='__main__':
 
     with open("data.pkl", "wb") as f:
         pickle.dump(data,f)
+    """
+    sample output:
+        [{'product': 'Մատանի «DF Project» արծաթյա №14',
+        'item_code': 'N:051649',
+        'store': 'DF project',
+        'brand': 'DF Project',
+        'url': 'https://4u.am/hy/product/matani-df-project-arcatya-no14',
+        'price': '9,000',
+        'item description': '925 հարգի արծաթյա մատանի:\nՉափը՝ կարգավորվող:\nՔարը՝ օնիքս։\nՔաշը՝ 3.5 գ։',
+        'image url': 'https://static.4u.am/origin/product/1024/dejhlp-iRIAG78l4.jpg'},
+        {'product': 'Բրոշ «LilmArt» աստղ',
+        'item_code': 'N:006014',
+        'store': 'LilmArt',
+        'url': 'https://4u.am/hy/product/bros-lilmart-astg',
+        'price': '4,400',
+        'item description': 'Բրոշը ամբոջությամբ ձեռքի աշխատանք է:\n\n\nՔարերը`բյուրեղ:\n\n\nԵրկարությունը` 4 սմ: \n\n\nԿարող եք պատվիրել Ձեր ցանկացած չափի:',
+        'image url': 'https://static.4u.am/origin/product/1024/0sTXJB1jHm82-zwM.jpg'},
+        {'product': 'Թևնոց «SOKOLOV» 94050369',
+        'item_code': 'N:074276',
+        'store': 'Charm.am',
+        'url': 'https://4u.am/hy/product/tevnoc-sokolov-94050369',
+        'price': '13,900',
+        'item description': 'Ապրանքանիշ` SOKOLOV\nՄետաղը՝ 925 հարգի արծաթ՝ ռոդիումապատ',
+        'image url': 'https://static.4u.am/origin/product/1024/5uIfr0JyNob8-wzp.jpg'},
+        {'product': 'Ականջօղեր «DF Project» լուսածին արծաթյա',
+        'item_code': 'N:000719',
+        'store': 'DF project',
+        ...
+        'store': 'THE BOX',
+        'url': 'https://4u.am/hy/product/nver-tup-the-box-no85-mankakan-agjka-hamar',
+        'price': '14,500',
+        'item description': 'Նվեր-տուփը ներառում է՝ \n\n\n- Սանր մանկական \n- Հայելի \n- Կերամիկական բաժակ \n- Գունավոր թուղթ\n- Քանոն միաեղջյուրով\n- Վարդագույն միաեղջյուրով գրիչ\n- Նոթատետր սպիտակ փայլիկներով\n- Դակիչ միաղջյուրով\n- Մարշմելո 100 գրամ \n\n\n\n\nՏուփի չափերը՝  \nԵրկարությունը՝ 19 սմ \nԼայնությունը՝ 26 սմ\nԲարձրությունը՝ 10 սմ\n\nԱպրանքները ենթակա են փոփոխության համարժեք գնով եւ տեսքով ապրանքների հետ:',
+        'image url': 'https://static.4u.am/origin/product/1024/Em3kAg3hhDY8406B.jpg'}]
+    """
