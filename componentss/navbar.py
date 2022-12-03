550f5635053f51de556c97f745dcbc2c9e98ce2c
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2022-12-03 13:13:40 +0400

    Add files via upload

--- componentss/navbar.py
+++ componentss/navbar.py
@@ -1,21 +1,20 @@
 from dash import html
 import dash_bootstrap_components as dbc
-from dash import dcc, ctx
 
 
-# Define the navbar structure
+# Defining the navbar structure
 def Navbar():
-
     layout = html.Div([
         dbc.NavbarSimple(
             children=[
-                dbc.Button("Home", outline=True, color="danger"),
-                dcc.Link(dbc.Button("Personalized", outline=True, color="primary"), href='/page1'),
-                dcc.Link(dbc.Button("Non-personalized", outline=True, color="success"), href='/page2')
-            ] ,
-            #brand="Multipage Dash App",
+                dbc.NavItem(dbc.NavLink(dbc.Button("Home", outline=True, color="danger"), active=True, href="/")),
+                # home button
+                dbc.NavItem(dbc.NavLink(dbc.Button("More", outline=True, color="primary"), href="/page1")),  # more page
+                dbc.NavItem(dbc.NavLink(dbc.Button("About us", outline=True, color="success"), href="/page2")),
+                # about us page
+
+            ],
             brand_href="/",
-            #color="dark",
             dark=True,
         ),
     ])
