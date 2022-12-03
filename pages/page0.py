a97ea3f15ac82576a234a7349ba372ee61222efd
HripyVoskanyan <75030550+HripyVoskanyan@users.noreply.github.com>
2022-12-03 13:12:21 +0400

    Add files via upload

---
+++ pages/page0.py
@@ -0,0 +1,96 @@
+# Import necessary libraries
+from data.metrics import *
+from dash import html
+from dash import dcc
+import dash_bootstrap_components as dbc
+
+# defining the card contents
+card_content1 = [
+    dbc.CardHeader("Average Time Spent in Website"),
+    dbc.CardBody(
+        [
+            html.H5(average_time, className="info"),
+
+        ]
+    ),
+]
+
+card_content2 = [
+    dbc.CardHeader("Total Revenue Gain"),
+    dbc.CardBody(
+        [
+            html.H5(total_revenue, className="info"),
+
+        ]
+    ),
+]
+
+card_content3 = [
+    dbc.CardHeader("Revenue per Click"),
+    dbc.CardBody(
+        [
+            html.H5(revenue_per_click, className="info"),
+
+        ]
+    ),
+]
+
+card_content4 = [
+    dbc.CardHeader("The rate of 'add to cart and buy'"),
+    dbc.CardBody(
+        [
+            html.H5(add_to_cart_rate, className="info"),
+
+        ]
+    ),
+]
+
+card_content5 = [
+    dbc.CardHeader("Revenue per minute"),
+    dbc.CardBody(
+        [
+            html.H5(revenue_per_minute, className="info"),
+
+        ]
+    ),
+]
+
+card_content6 = [
+    dbc.CardHeader("Conversion Rate"),
+    dbc.CardBody(
+        [
+            html.H5(conversion_rate, className="info"),
+
+        ]
+    ),
+]
+
+# turning the cards into two rows
+row_1 = dbc.Row(
+    [
+        dbc.Col(dbc.Card(card_content1, color="primary", outline=True), width=3),
+        dbc.Col(dbc.Card(card_content2, color="secondary", outline=True), width=3),
+        dbc.Col(dbc.Card(card_content3, color="info", outline=True), width=3),
+    ],
+    className="mb-4",
+)
+
+row_2 = dbc.Row(
+    [
+        dbc.Col(dbc.Card(card_content4, color="success", outline=True), width=3),
+        dbc.Col(dbc.Card(card_content5, color="warning", outline=True), width=3),
+        dbc.Col(dbc.Card(card_content6, color="danger", outline=True), width=3),
+    ],
+    className="mb-4",
+)
+
+# defining the layout
+layout = html.Div([
+    dbc.Row([
+        dbc.Col(html.Div([row_1, row_2])),
+        dbc.Col([
+            dcc.Graph(figure=fig1
+                      )  # figure 1 called from metrics
+        ]
+        ),
+    ])]),
--- pages/page2.py
+++ pages/page2.py
@@ -1,35 +1,24 @@
 # Import necessary libraries
-import dash
 from dash import html
 import dash_bootstrap_components as dbc
 
-### Add the page components here
-table_header = [
-    html.Thead(html.Tr([html.Th("First Name"), html.Th("Last Name")]))
-]
 
-row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
-row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
-row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
-row4 = html.Tr([html.Td("Trillian"), html.Td("Astra")])
-
-table_body = [html.Tbody([row1, row2, row3, row4])]
-
-page2_table = dbc.Table(table_header + table_body, bordered=True)
-
-# Define the final page layout
-layout = dbc.Container([
-    dbc.Row([
-        html.Center(html.H1("Page 2")),
-        html.Br(),
-        html.Hr(),
-        dbc.Col([
-            html.P("This is column 1."),
-            dbc.Button("Test Button", color="secondary")
+# defining the layout
+layout = html.Div([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1("Recommendation System for 4u.am"), className="mb-2")
         ]),
-        dbc.Col([
-            html.P("This is column 2."),
-            page2_table
-        ])
-    ])
-])
+        dbc.Row([
+            dbc.Col(
+                html.H6(['The recommendation system for 4u.am helps people who use 4u.am and want to get ', html.Br(),
+                         'personalized recommendations and make correct decisions in their online ', html.Br(),
+                         'shopping. Our main users are people who already use 4u.am, or are starting to ', html.Br(),
+                         'use online shopping tools. For 4u.am the recommendation system will increase ', html.Br(),
+                         'sales and redefine the users\' web browsing experience, retain the customers, ', html.Br(),
+                         'and enhance their shopping experience.']), className="mb-4")
+        ]),
+        dbc.Row(dbc.Col([html.H6('For more information and source code:'),
+                         html.A(html.H6('here'), href='https://github.com/anryabrahamyan/Recommender_system_4u.am)')]))
+
+    ])])
